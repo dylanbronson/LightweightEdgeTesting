cf9cee7f152abcd24e3abddfce3259bbc28ef0d3
dylanbronson <dybronso@microsoft.com>
2020-02-25 10:37:39 -0800

    Adding testProcessManager

---
+++ TestProcessManager/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace TestProcessManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Running file found at ./testFile");
+            ProcessStartInfo startInfo = new ProcessStartInfo() { FileName = "./testFile"};
+            Process proc = new Process() { StartInfo = startInfo, };
+            proc.Start();
+        }
+    }
+}
